ac8925b4029249a21ed2bb7c2a2d7c4ffe4907fa
Ale mae <deadun99@gmail.com>
2025-10-05 18:04:39 +0300

    второй commit

--- src/ExampleApplication.java
+++ src/ExampleApplication.java
@@ -1,5 +1,5 @@
 public class ExampleApplication {
     public static void main(String[] args) {
-        System.out.println("ну чо каво");
+        System.out.println("что программист шоле?");
     }
 }
